95cb9e0891a22f80552c99b945bd7239689e7d15
C23598 <C23598@L-PC0UFPZ8.us.int.rci.com>
2020-04-12 19:51:14 -0400

    changes fortype

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,15 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @EnableAutoConfiguration
-public class Example {
+public class Example<T> {
 
     @RequestMapping("/")
-    String home() {
-        return "Hello World - test1!  build complete";
+    public ResponseEntity<List<Integer>> home() {
+    	List<Integer> testList =  new ArrayList<Integer>();
+    	testList.add(10);
+    	testList.add(11);
+
+    	testList.add(12);
+
+    	testList.add(6);
+
+    	testList.add(13);
+
+    	testList.add(10);
+
+        return ResponseEntity.ok(testList.stream().filter(s->s>6).collect(Collectors.toList()));
     }
 
     public static void main(String[] args) throws Exception {
